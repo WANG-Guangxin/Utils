914c472aad4ee9a9d2839cfa540a64ced42e1571
Guangxin Wang (汪广鑫) <71872849+WANG-Guangxin@users.noreply.github.com>
2022-02-09 10:24:34 +0800

    自动分享 阿里云盘文件

--- share_ali.py
+++ share_ali.py
@@ -1,12 +1,12 @@
 from aligo import Aligo
 import sys
-import datetime
+import datetime 
 
 ali = Aligo(refresh_token=sys.argv[1])
 share = ali.share_file(['5fdb5f561c7085c9fc0a42ef9965b157708c0876'],
                        share_name='临时文件',
                       share_pwd='963258',
-                      expiration=str(datetime.datetime.now()+datetime.timedelta(days=1)),
+                      expiration=str(datetime.datetime.now()+datetime.timedelta(hours=12)),
                       description='自动分享')
 
 print(share)
