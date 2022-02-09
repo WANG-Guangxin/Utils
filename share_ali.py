b6dbb76cc5a74321d0cef40c1f5b54d1c53aadb1
Guangxin Wang (汪广鑫) <71872849+WANG-Guangxin@users.noreply.github.com>
2022-02-09 10:39:20 +0800

    Update share_ali.py

--- share_ali.py
+++ share_ali.py
@@ -6,7 +6,7 @@ ali = Aligo(refresh_token=sys.argv[1])
 share = ali.share_file(['5fdb5f561c7085c9fc0a42ef9965b157708c0876'],
                        share_name='临时文件',
                       share_pwd='963258',
-                      expiration=str(datetime.datetime.now()+datetime.timedelta(hours=12)),
+                      expiration=str((datetime.datetime.now()+datetime.timedelta(hours=12)).strftime("%Y-%m-%dT%H:%M:%S.000Z")),
                       description='自动分享')
 
 print(share)
