0fca39e7644a5919a033664b8a5dce6b5c3d4b09
Guangxin Wang (汪广鑫) <71872849+WANG-Guangxin@users.noreply.github.com>
2024-07-21 09:14:23 +0800

    Add files via upload

---
+++ server_init_lazybones.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 更新系统
+apt-get update
+
+# 安装 iptables
+apt-get install -y iptables
+
+# 设置 iptables 规则
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+
+# 保存 iptables 规则
+iptables-save
+
+# 安装 iptables-persistent 并保存规则
+apt-get install -y iptables-persistent
+netfilter-persistent save
+netfilter-persistent reload
+
+# 复制 authorized_keys 文件
+cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/
+
+# 删除 60-cloudimg-settings.conf 文件
+rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+
+# 修改 sshd_config 文件以允许 root 登录和密码认证
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+# 设置 root 密码
+echo "Please enter the new root password:"
+read -s root_password
+echo "root:${root_password}" | chpasswd
+
+# 重启 sshd 服务
+systemctl restart sshd
